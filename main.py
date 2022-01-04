1c58be75db788ec72ae088c3b4f8d51fd969b0f5
Adam Fraga <adam.fraga@laplateforme.io>
2022-01-04 16:52:14 +0100

    Clean comments

--- main.py
+++ main.py
@@ -2,22 +2,45 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
-# !!!WARNING!!!
-# Fast api read the first path request if that match the next are skip
+# !!!IMPORTANT!!!
+# Fast api read the first path request if that match the next are skip.
+# Each time modify the return we have to relaunch uvicorn server.
+# --reload allow uvicorn to watch for changes.
+
 
 app = FastAPI()
 
+"""
+PYDANTIC:
+  Allow to use Schema for our API to define the type of data we excepts from users.
+  Can also throw errors if the type not match.
+  Convert the result into an object to facilite the access of the data.
+  Provide à dict() Method to convert the object in a dictionnary.
+"""
 
-# Pydantic allow use to use Schema for our API to define the type of data we excepts from users
-# Pydantic can also throw errors if the type not match
+
 class Post(BaseModel):
     title: str
     content: str
-    # Provide optionnal value if no value send default is True
     published: bool = True
-    # Provide optionnal value of type int if no value send = None
     rating: Optional[int] = None
+
+
+# Post test (Substitute to table SQL)
+my_posts = [{"title": "title of post 1",
+             "content": "content of post 1", "id": 1},
+            {"title": "title of post 2",
+             "content": "content of post 2", "id": 2}]
+
+"""
+    Utilitie function (substitute to SQL select request )
+"""
+
+
+def find_post(id):
+    return my_posts[id]
 
 
 """ 
@@ -29,8 +52,6 @@ Returns: Python dictionnary convert to JSON
 
 @app.get('/')
 def root():
-    # Each time modify the return we have to relaunch uvicorn server
-    # --reload allow uvicorn to watch changes
     return {"message": "Hello world!!"}
 
 
@@ -43,30 +64,52 @@ Returns: Python dictionnary convert to JSON
 
 @app.get('/posts')
 def method_name():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 
 """ 
 POST method
-Arguments: Path to posts
-Returns: Python dictionnary convert to JSON
+arguments decorators: path to posts
+
+!!!PREFERE EXAMPLE 2 WITH PYDANTICS!!!
+arguments method (example 1): post = Catch the request body response convert it into a dictionnary
+arguments method (example 2): post = Use Pydantics Model 
+
+returns: python dictionnary convert to json
+
+!!!NOTES!!!
+(the printed value are displayed in uvicorn server)
+(The return Value are displayed the data in a python dictionnary convert to JSON)
+"""
+
+# EXAMPLE 1
+# @app.post('/posts')
+# def create_post(post: dict = Body(...)):
+#     print(post)
+#     return {"new_post": f"title: {post['title']} content: {post['content']}"}
+
+
+# EXAMPLE 2
+@app.post('/posts')
+def create_post(post: Post):
+    # print(post.title)
+    # print(post.dict())
+    # We need an id for our post randrange provide is for us
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 10000000)
+    # Append new post to my_posts (simulate BDD)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+""" 
+GET method
+decorator arguments: path to posts
+method arguments: id (automaticaly catch from the path)
+returns: python dictionnary convert to JSON
 """
 
 
-@app.post('/posts')
-# Stock Post data into a dictionnary variable named payLoad Body is import from Fastapi
-# def create_post(payLoad: dict = Body(...)):
-# display the data in uvicorn server
-# print(payLoad)
-# display the data in a python dictionnary (JSON)
-# return {"new_post": f"title: {payLoad['title']} content: {payLoad['content']}"}
-# OR with pydantic Model
-# Post here reference our pydantic Model for our Post we except a content ant a title both (str)
-# Here before the execution the moel is checked and it throws error for users to inform them of
-# what data the API is excepting from them
-def create_post(my_post: Post):
-    # my_post is now an object of type post and allow us to access easly the data
-    print(my_post.title)
-    # Pydantic allow us to convert a Pydantic model to a dictionnary with dict method
-    print(my_post.dict())
-    return {"data": my_post}
+@app.get('/posts/{id}')
+def get_post(id):
+    return {"post": find_post(id)}
